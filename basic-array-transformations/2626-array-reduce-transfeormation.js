38593365ed15be782edb640b69105f49fb900a1f
Vitor Caetano <vitorkaetano343@gmail.com>
2026-01-15 14:50:27 -0300

    Problem solution

---
+++ basic-array-transformations/2626-array-reduce-transfeormation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let ans = 0;
+
+    for (i=0;i<nums.length;i++)
+};
